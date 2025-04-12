264142506002dc360d2acdaf85d28e3ad3ccd88d
abhijith-ms <abhijithms.9526@gmail.com>
2025-04-12 11:47:36 +0530

    Added sitemap.xml

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import Hero from '../components/Hero'
 import HomeServices from '../components/HomeServices' // Updated import
 import WhyChooseUs from '../components/WhyChooseUs'
 
+
+
 export default function Home() {
   return (
     <div>
